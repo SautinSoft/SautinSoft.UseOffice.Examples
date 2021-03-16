37b608b8051e476c6a401382254c86d104c2ac91
SautinSoftSupport <79837963+SautinSoftSupport@users.noreply.github.com>
2021-03-16 13:41:55 +0400

    Add files via upload

---
+++ Convert PDF to DOC - only Office 2013 and higher/sample.cs
@@ -0,0 +1,53 @@
+using System;
+using System.IO;
+using System.Collections;
+using SautinSoft;
+
+namespace Sample
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            // Convert PDF file to DOC file. Works only in Office 2013 and higher.
+            
+            // If you are looking for solution without MS Office
+            // Please take a look at our PDF Focus .Net: https://www.sautinsoft.com/products/pdf-focus/index.php
+
+            SautinSoft.UseOffice u = new SautinSoft.UseOffice();
+
+            string inpFile = Path.GetFullPath(@"..\..\..\..\..\Testing files\example.pdf");
+            string outFile = Path.GetFullPath("Result.doc");
+
+            // Prepare UseOffice .Net, loads MS Word in memory
+            if (u.InitWord() != 0)
+            {
+                Console.WriteLine("Error: Can't load MS Word in memory!");
+                Console.WriteLine("Please contact SautinSoft's support Team: support@sautinsoft.com.");
+                Console.ReadLine();
+            }
+
+            // Check MS Office version
+            if (u.OfficeVersion >= UseOffice.eOfficeVersion.Office2013)
+            {
+                // Converting ...
+                int result = u.ConvertFile(inpFile, outFile, UseOffice.eDirection.PDF_to_DOC);
+
+                if (result == 0)
+                {
+                    Console.WriteLine("Converting successfully!");
+                    // Open the result.
+                    System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(outFile) { UseShellExecute = true });
+
+                }
+                else
+                    Console.WriteLine("Error! Please contact with SautinSoft support: support@sautinsoft.com.");
+            }
+            else
+            {
+                Console.WriteLine("To convert PDF documents, please install MS Office 2013 or higher.");
+            }
+            u.CloseOffice();
+        }
+    }
+}
